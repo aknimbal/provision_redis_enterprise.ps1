99aef71c8a3203ed671829b7a997c6f0167e18aa
Akshay Nimbalkar <48735789+aknimbal@users.noreply.github.com>
2025-09-19 23:36:37 -0400

    Refactor Redis Enterprise provisioning script
    Updated variables for Redis Enterprise provisioning and streamlined the cluster creation process.

--- provision_redis_enterprise.ps1
+++ provision_redis_enterprise.ps1
@@ -2,81 +2,22 @@
 # Set variables
 $SUBSCRIPTION_ID = $(az account show --query id -o tsv)
 
-$CLUSTER_EAST = "ai-cx-n-caching-eastus-rc"
-$CLUSTER_WEST = "ai-cx-n-caching-westus-rc"
-$LOCATION_EAST = "EastUS"
-$LOCATION_WEST = "WestUS"
-$SKU = "Enterprise_E10"
+$SUBSCRIPTION_ID = "66816faa-eefa-4b9c-9bd6-79c5de539b3b"
+$CLUSTER_EAST = "aks-ai-cx-n-caching-eastus-rc"
+$CLUSTER_WEST = "aks-ai-cx-n-caching-westus-rc"
+$LOCATION_EAST = "East US"
+$LOCATION_WEST = "West US"
+$SKU = "Balanced_B10"
 $DB_NAME = "default"
 $GROUP_NICKNAME = "redis-geo-group"
-$RESOURCE_GROUP = "ai-cx-n-hydapmtest-rg"
+$RESOURCE_GROUP = "myRedisRG"
 
-# Select subscription
-Select-AzSubscription -SubscriptionId $SUBSCRIPTION_ID
+# Login and set subscription
+az login
+az account set --subscription $subscriptionId
 
-# Create resource group if it doesn't exist
-if (-not (Get-AzResourceGroup -Name $RESOURCE_GROUP -ErrorAction SilentlyContinue)) {
-    Write-Host "Creating resource group..."
-    New-AzResourceGroup -Name $RESOURCE_GROUP -Location $LOCATION_EAST
-} else {
-    Write-Host "Resource group already exists."
-}
+# Create Redis Enterprise clusters in EAST and linked database
+az redisenterprise create --location $LOCATION_EAST --cluster-name $CLUSTER_EAST --sku $SKU --resource-group $RESOURCE_GROUP --group-nickname $GROUP_NICKNAME --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_EAST/databases/$DB_NAME" 
 
-Write-Host "Using subscription: $SUBSCRIPTION_ID"
-
-# Create Redis Enterprise clusters
-Write-Host "Creating Redis Enterprise cluster in East US..."
-az redisenterprise create `
-  --name $CLUSTER_EAST `
-  --resource-group $RESOURCE_GROUP `
-  --location $LOCATION_EAST `
-  --sku $SKU
-
-Write-Host "Creating Redis Enterprise cluster in West US..."
-az redisenterprise create `
-  --name $CLUSTER_WEST `
-  --resource-group $RESOURCE_GROUP `
-  --location $LOCATION_WEST `
-  --sku $SKU
-
-# Wait for clusters to be ready
-Write-Host "Waiting for clusters to be ready..."
-Start-Sleep -Seconds 60
-
-# Create databases in each cluster
-Write-Host "Creating database in East US cluster..."
-az redisenterprise database create `
-  --cluster-name $CLUSTER_EAST `
-  --resource-group $RESOURCE_GROUP `
-  --name $DB_NAME
-
-Write-Host "Creating database in West US cluster..."
-az redisenterprise database create `
-  --cluster-name $CLUSTER_WEST `
-  --resource-group $RESOURCE_GROUP `
-  --name $DB_NAME
-
-# Wait for databases to be ready
-Write-Host "Waiting for databases to be ready..."
-Start-Sleep -Seconds 60
-
-# Enable active geo-replication
-Write-Host "Setting up geo-replication..."
-$EAST_DB_ID = "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_EAST/databases/$DB_NAME"
-$WEST_DB_ID = "/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_WEST/databases/$DB_NAME"
-
-az redisenterprise database update `
-  --cluster-name $CLUSTER_EAST `
-  --resource-group $RESOURCE_GROUP `
-  --name $DB_NAME `
-  --linked-database-id $WEST_DB_ID `
-  --group-nickname $GROUP_NICKNAME
-
-az redisenterprise database update `
-  --cluster-name $CLUSTER_WEST `
-  --resource-group $RESOURCE_GROUP `
-  --name $DB_NAME `
-  --linked-database-id $EAST_DB_ID `
-  --group-nickname $GROUP_NICKNAME
-
-Write-Host "Azure Managed Redis Enterprise multi-region with geo-replication provisioned."
+# Create Redis Enterprise clusters in WEST and create a linked database to EAST
+az redisenterprise create --location $LOCATION_WEST --cluster-name $CLUSTER_WEST --sku $SKU --resource-group $RESOURCE_GROUP --group-nickname $GROUP_NICKNAME --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_EAST/databases/default" --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_WEST/databases/$DB_NAME" 
