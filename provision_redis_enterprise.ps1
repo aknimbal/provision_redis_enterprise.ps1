94fee2e0d7140e966b734c355e1660b9c61246e0
Akshay Nimbalkar <48735789+aknimbal@users.noreply.github.com>
2025-09-24 18:39:40 -0400

    Enhance Redis Enterprise provisioning script
    Added clustering policy and module configuration to Redis Enterprise cluster creation commands.

--- provision_redis_enterprise.ps1
+++ provision_redis_enterprise.ps1
@@ -9,13 +9,14 @@ $SKU = "Balanced_B10"
 $DB_NAME = "default"
 $GROUP_NICKNAME = "redis-geo-group"
 $RESOURCE_GROUP = "myRedisRG"
+$MODULE_NAME = "RediSearch"
 
 # Login and set subscription
 az login
 az account set --subscription $subscriptionId
 
 # Create Redis Enterprise clusters in EAST and linked database
-az redisenterprise create --location $LOCATION_EAST --cluster-name $CLUSTER_EAST --sku $SKU --resource-group $RESOURCE_GROUP --group-nickname $GROUP_NICKNAME --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_EAST/databases/$DB_NAME" 
+az redisenterprise create --location $LOCATION_EAST --cluster-name $CLUSTER_EAST --sku $SKU --resource-group $RESOURCE_GROUP --group-nickname $GROUP_NICKNAME --clustering-policy EnterpriseCluster --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_EAST/databases/$DB_NAME" --modules name=$MODULE_NAME --eviction-policy "NoEviction"
 
 # Create Redis Enterprise clusters in WEST and create a linked database to EAST
-az redisenterprise create --location $LOCATION_WEST --cluster-name $CLUSTER_WEST --sku $SKU --resource-group $RESOURCE_GROUP --group-nickname $GROUP_NICKNAME --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_EAST/databases/$DB_NAME" --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_WEST/databases/$DB_NAME" 
+az redisenterprise create --location $LOCATION_WEST --cluster-name $CLUSTER_WEST --sku $SKU --resource-group $RESOURCE_GROUP --group-nickname $GROUP_NICKNAME --clustering-policy EnterpriseCluster --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_EAST/databases/$DB_NAME" --linked-databases id="/subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP/providers/Microsoft.Cache/redisEnterprise/$CLUSTER_WEST/databases/$DB_NAME" --modules name=$MODULE_NAME --eviction-policy "NoEviction" 
